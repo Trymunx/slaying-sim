1603ed3420aef5e41f0e603951285d3757b4d0fc
Trymunx <trymunx@gmail.com>
2017-08-07 00:03:02 +0100

    Adds some functions for dragon attacks and potion drinking, but still doesn't work yet.

--- DragonGame_v2.js
+++ DragonGame_v2.js
@@ -7,8 +7,9 @@ var dragon = {
   currentHP: 0,
   attackType: "",
   missAttack: 0,
+  maulAttack: getRandomInt(1,12),
   breathAttack: getRandomInt(8,35),
-  maulAttack: getRandomInt(1,12)
+  spawnDragon: spawnDragon,
 }
 
 var player = {
@@ -17,10 +18,46 @@ var player = {
   experience: 0,
   attackChance: Math.round(Math.random()),
   dragonsSlain: 0,
-  damgage: getRandomInt(1, 4500)
+  damgage: getRandomInt(1, 4500),
+  potions: 5
 }
 
 function spawnDragon() {
-  dragon.totalHP = getRandomInt(15000, 50000);
-  dragon.currentHP = dragon.totalHP;
+  this.totalHP = getRandomInt(15000, 50000);
+  this.currentHP = this.totalHP;
+}
+
+function getDragonAttackType {
+  var attack = Math.random();
+  if (attack < 0.5) {
+    dragon.attackType = 0;
+    var message = Math.random();
+    switch(message){
+      case (message < 0.35):
+        console.log("You raise your shield and block the dragon's attack.");
+        break;
+      case (message < 0.60):
+        console.log("You sidestep the dragon's attack.");
+        break;
+      default:
+        console.log("The dragon's attack misses you.");
+    }
+  } else if (attack < 0.85) {
+    dragon.attackType = 1;
+    console.log("The dragon mauls you for " + dragon.maulAttack + " damage.");
+  } else {
+    dragon.attackType = 2;
+    console.log("You raise your shield but the dragon breaths fire, hitting you for " + dragon.breathAttack + "HP.")
+  }
+}
+
+function drinkPotion() {
+  if (player.potions > 0) {
+    player.currentHP += 20;
+    player.potions -= 1;
+    console.log("You drink a potion, restoring 20HP. You have " + player.potions + " potions remaining.");
+  } else {
+    console.log("You have no potions left to drink!");
+  }
+  console.log("You have " + player.currentHP + "HP remaining.");
 }

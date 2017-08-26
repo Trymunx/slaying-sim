d6259fa2b4116062d9ca37e4432a709ffe38ff05
Trymunx <trymunx@gmail.com>
2017-08-26 19:11:15 +0100

    Fix path to Game_v3 so can still use script to run it

--- runGame.sh
+++ runGame.sh
@@ -4,4 +4,4 @@ mkdir -p output
 X=$( ls -l output/ | grep -v ^d | wc -l )
 Y=$(( X - 1 ))
 echo "Outputting to output$Y.txt"
-node Game_v3.js > output/output$Y.txt
+node old_versions/Game_v3.js > output/output$Y.txt

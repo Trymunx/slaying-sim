3052e33f3579857bd3dcdd5f475a0968fbd46792
Trymunx <trymunx@gmail.com>
2017-08-28 23:40:18 +0100

    Add output for what is on the new tile and draws new map

--- src/movePlayer.js
+++ src/movePlayer.js
@@ -1,13 +1,6 @@
-function getPlayerPosition(map) {
-  var position;
-  for (let i = 0; i < map.length; i++) {
-    if (map[i].playerIsHere) {
-      position = i;
-      break;
-    }
-  }
-  return position;
-}
+const getPlayerPosition = require("./PlayerPosition.js");
+const drawMap = require("./drawMap.js");
+const look = require("./Look.js");
 
 function movePlayer(map, direction) {
   var sideLength = Math.sqrt(map.length);
@@ -19,28 +12,40 @@ function movePlayer(map, direction) {
     case "north":
       if (playerPos - sideLength < 0) {
         console.log("You can't move any further North!");
+      } else if (map[(playerPos - sideLength)].terrain === "river" || map[(playerPos - sideLength)].terrain === "bridgeUpper" || map[(playerPos - sideLength)].terrain === "bridgeLower") {
+        console.log("You can't cross the river.");
       } else {
+        console.log("Moving North...");
         playerPos -= sideLength;
       }
       break;
     case "south":
       if (playerPos + sideLength > map.length) {
         console.log("You can't move any further South!");
+      } else if (map[(playerPos + sideLength)].terrain === "river" || map[(playerPos + sideLength)].terrain === "bridgeUpper" || map[(playerPos + sideLength)].terrain === "bridgeLower") {
+        console.log("You can't cross the river.");
       } else {
+        console.log("Moving South...");
         playerPos += sideLength;
       }
       break;
     case "east":
       if ((playerPos + 1) % sideLength === 0) {
         console.log("You can't move any further East!");
+      } else if (map[(playerPos + 1)].terrain === "river" || map[(playerPos + 1)].terrain === "bridgeUpper" || map[(playerPos + 1)].terrain === "bridgeLower") {
+        Console.log("You can't cross the river.");
       } else {
+        console.log("Moving East...");
         playerPos++;
       }
       break;
     case "west":
       if (playerPos % sideLength === 0) {
         console.log("You can't move any further West!");
+      } else if (map[(playerPos - 1)].terrain === "river" || map[(playerPos - 1)].terrain === "bridgeUpper" || map[(playerPos - 1)].terrain === "bridgeLower") {
+        Console.log("You can't cross the river.");
       } else {
+        console.log("Moving West...");
         playerPos--;
       }
       break;
@@ -52,6 +57,9 @@ function movePlayer(map, direction) {
   // Move player to the new position
   map[oldPlayerPos].playerIsHere = false;
   map[playerPos].playerIsHere = true;
+
+  drawMap(map);
+  look(map);
 }
 
 module.exports = movePlayer;

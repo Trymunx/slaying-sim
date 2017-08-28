87c10e75d8ea695db96be3200447188c8b7dbf99
Trymunx <trymunx@gmail.com>
2017-08-28 23:39:53 +0100

    Add rivers

--- src/mapGen.js
+++ src/mapGen.js
@@ -44,18 +44,109 @@ function genMap(sideLength) {
       "playerIsHere": false,
       "creature": [],
       "items": [],
-      "structures": []
+      "structures": [],
+      "terrain": []
     };
   }
 
-  // Generate up to one creature per tile
+  // Generate terrain first
+
+  // RIVERS - only spawn 60% of the time
+  if (RNG() < 0.6) {
+
+    // Bridge appears first
+    var bridgeMain = Math.round(RNG(mapSize-1));
+    var bridgePlaced = false;
+    while (!bridgePlaced) {
+      if (bridgeMain % sideLength === 0 || (bridgeMain + 1) % sideLength === 0) {
+        bridgeMain = Math.round(RNG(mapSize-1));
+      } else {
+        map[bridgeMain].terrain = "bridge";
+        bridgePlaced = true;
+      }
+    }
+    // Add | | above bridge if not at the top of map
+    if (bridgeMain - sideLength > 0) {
+      var bridgeUpper = bridgeMain - sideLength;
+      map[bridgeUpper].terrain = "bridgeUpper";
+    }
+    // Add | | below bridge if not at bottom of map
+    if (bridgeMain + sideLength < (mapSize - 1)) {
+      var bridgeLower = bridgeMain + sideLength;
+      map[bridgeLower].terrain = "bridgeLower";
+    }
+
+    // River extends upwards from bridge
+    var riverAtTop = false;
+    while (!riverAtTop) {
+      // Is the bridge or bridge upper at the top of the map?
+      if (bridgeMain - (sideLength * 2) < 0) {
+        riverAtTop = true;
+      } else { // Extend the river upwards
+        var lastRiverTile = bridgeUpper;
+        while (lastRiverTile - sideLength > 0) {
+          let riverTile = Math.round(RNG(lastRiverTile - sideLength - 1, lastRiverTile - sideLength + 1));
+          map[riverTile].terrain = "river";
+          lastRiverTile = riverTile;
+        }
+        riverAtTop = true;
+      }
+    }
+
+    // River extends downwards from bridge
+    var riverAtBottom = false;
+    while (!riverAtBottom) {
+      // Is the bridge or bridge lower at the bottom of the map?
+      if (bridgeMain + (sideLength * 2) > (map.length - 1)) {
+        riverAtBottom = true;
+      } else { // Extend the river downwards
+        var lastRiverTile = bridgeLower;
+        while (lastRiverTile + sideLength < (map.length - 1)) {
+          let riverTile = Math.round(RNG(lastRiverTile + sideLength - 1, lastRiverTile + sideLength + 1));
+          map[riverTile].terrain = "river";
+          lastRiverTile = riverTile;
+        }
+        riverAtBottom = true;
+      }
+    }
+
+
+  //   // Starting position somewhere along top of map
+  //   var riverStart = Math.round(RNG(sideLength-1));
+  //   map[riverStart].terrain = "river";
+  //   var lastRiverTile = riverStart;
+  //   var riverTile;
+  //
+  //   for (i = 1; i < sideLength; i++) {
+  //     if (lastRiverTile % sideLength === 0) { // last tile is on the left edge of the map
+  //       riverTile = Math.round(RNG((lastRiverTile + sideLength), (lastRiverTile + sideLength + 1)));
+  //       map[riverTile].terrain = "river";
+  //       lastRiverTile = riverTile;
+  //     } else if ((lastRiverTile + 1) % sideLength === 0) { // last tile is on the right edge of map
+  //       riverTile = Math.round(RNG((lastRiverTile + sideLength - 1), (lastRiverTile + sideLength)));
+  //       map[riverTile].terrain = "river";
+  //       lastRiverTile = riverTile;
+  //     } else { // Start is not at an edge
+  //       riverTile = Math.round(RNG((lastRiverTile + sideLength - 1), (lastRiverTile + sideLength + 1)));
+  //       map[riverTile].terrain = "river";
+  //       lastRiverTile = riverTile;
+  //     }
+  //   }
+  }
+
+  // Generate up to one creature per empty tile
   for (i=0; i < mapSize; i++) {
-    // Check to see if there is a creature on that tile, if not set to null
-    var spawnedCreature = spawnCreature();
-    if (spawnedCreature) {
-      map[i].creature = newCreature(spawnedCreature);
-    } else {
+    // Check to see if the tile has a river
+    if (map[i].terrain.length !== 0) {
       map[i].creature = null;
+    } else {
+      // Check to see if there is a creature on that tile, if not set to null
+      var spawnedCreature = spawnCreature();
+      if (spawnedCreature) {
+        map[i].creature = newCreature(spawnedCreature);
+      } else {
+        map[i].creature = null;
+      }
     }
   }
 
@@ -63,7 +154,7 @@ function genMap(sideLength) {
   var randPos = Math.round(RNG(mapSize-1));
   var playerPlaced = false;
   while (!playerPlaced) {
-    if (map[randPos].creature !== null) {
+    if (map[randPos].creature !== null || map[randPos].terrain === true) {
       randPos = Math.round(RNG(mapSize-1));
     } else {
       map[randPos].playerIsHere = true;

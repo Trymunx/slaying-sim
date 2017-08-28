dab8148364b2439010eb2a0fe4078def87951d3b
Trymunx <trymunx@gmail.com>
2017-08-28 23:41:22 +0100

    Add ability to print what is on the current tile

---
+++ src/Look.js
@@ -0,0 +1,15 @@
+const getPlayerPosition = require("./PlayerPosition.js");
+
+function look(map) {
+  var playerPos = getPlayerPosition(map);
+
+  if (map[playerPos].creature) {
+    console.log("There is a " + map[playerPos].creature.name + " here.");
+  } else if (map[playerPos].terrain === "bridge") {
+    console.log("You are standing on a bridge.");
+  } else {
+    console.log("You look around but see nothing here.");
+  }
+}
+
+module.exports = look;

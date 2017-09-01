b3416c043ac381f7709bc60231eac9cc06490aeb
Trymunx <trymunx@gmail.com>
2017-09-01 02:33:21 +0100

    Work in progress to add autonomous gameplay

--- Game_v4.js
+++ Game_v4.js
@@ -6,22 +6,41 @@ const drawMap = require("./src/drawMap.js");
 const movePlayer = require("./src/movePlayer.js");
 const getPlayerPosition = require("./src/PlayerPosition.js");
 const look = require("./src/Look.js");
+const getDirection = require("./src/Direction.js");
+const getLiveCreatures = require("./src/LiveCreatures.js");
 
+// Create a new player
 var player = newPlayer();
-
 console.log(player.name);
 
-console.log("Generating a map: ...");
-var map = genMap(Math.round(RNG(5,20)));
-drawMap(map);
-// Test move command
-movePlayer(map, "north");
+// Keep playing until the player is dead
+while (player.attributes.currentHP > 0) {
 
+  // Generate a map for the player
+  console.log("You awaken in a forest. Looking around, you see nothing but trees.");
+  var map = genMap(Math.round(RNG(5,20)));
+  var liveCreatures = getLiveCreatures(map);
+  drawMap(map);
 
+  // Generate a new map once all aggresive creatures are dead
+  while (liveCreatures.aggresive > 0) {
 
+    // If player is next to an aggressive creature, creature will attack
+    playerPos = getPlayerPosition(map);
+    currentCreature = map[playerPos].creature;
+    if (currentCreature.attributes.aggressive === true) {
+      player.attributes.currentHP = fight(currentCreature);
+    } else if (currentCreature.attributes.aggressive === false) {
+      console.log("The " + currentCreature + " looks harmless. You leave it alone.");
+    } else {
+      console.log("There's nothing but trees here!");
+    }
 
-// function goldDrop(creature) {
-//   var goldDropMiddleValue;
-//   goldDropMiddleValue = dimRNG(1,11,3.5) * creature.drops.gold.multiplier;
-//   console.log(goldDropMiddleValue);
-// }``
+    movePlayer(map, getDirection());
+  }
+
+  // Message before generating a new map
+  console.log("You suddenly feel very peculiar. Your world turns hazy, then " +
+                "things start to fade. You decide to rest your eyes for a while.");
+
+}
